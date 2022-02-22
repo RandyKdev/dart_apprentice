9feea8c7be207185be6df9b9fa7fab400a9900f6
Randy Kwalar <rkwalar@gmail.com>
2022-02-22 23:27:29 +0100

    Add chars mini exercise in chapter 3

---
+++ chapter_3/mini_exercises/chars/bin/chars.dart
@@ -0,0 +1,19 @@
+void main(List<String> arguments) {
+  print('Without characters package');
+  print('Hello');
+  print('Hello'.codeUnits);
+  print('Hello'.length);
+  print('Hello'.runes);
+  print('🎯');
+  print('🎯'.codeUnits);
+  print('🎯'.length);
+  print('🎯'.runes);
+  print('🇲🇳');
+  print('🇲🇳'.codeUnits);
+  print('🇲🇳'.length);
+  print('🇲🇳'.runes);
+  print('👨‍👩‍👧‍👦');
+  print('👨‍👩‍👧‍👦'.codeUnits);
+  print('👨‍👩‍👧‍👦'.length);
+  print('👨‍👩‍👧‍👦'.runes);
+}
